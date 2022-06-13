a5ea39ba187d51a38f67827e1619ab0c948e938a
Aleksandr Vinokhodov <vinokhodovalex@gmail.com>
2022-06-13 00:46:32 -0400

    auth WIP:laravel/ui;laraver/sanctum + cors

---
+++ app/Http/Controllers/API/UsersController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class UsersController extends Controller
+{
+    public function __construct() {
+        $this->middleware('auth:sanctum')->only('show');
+    }
+    public function show(Request $request)
+    {
+        return response()->json($request->user());
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::post('/register', 'Auth\AuthController@register');
+Route::post('/login', 'Auth\AuthController@login');
+Route::post('/logout', 'Auth\AuthController@logout');
+
+Route::get('/user', 'API\UserController@show');
